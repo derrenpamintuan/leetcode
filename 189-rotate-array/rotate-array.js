bce111fc101948b0ccfb5a2e0f2328d083fee31e
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-11 19:52:45 -0700

    Time: 88 ms (29.21%) | Memory: 58.4 MB (47.20%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,11 +4,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  for (let i = nums.length - 1; i >= 0; i--) {
-    nums[i + k] = nums[i];
+  function reverse(arr, start, end) {
+    while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
   }
 
-  for (let j = k - 1; j >= 0; j--) {
-    nums[j] = nums.pop();
-  }
+  k %= nums.length;
+
+  reverse(nums, 0, (nums.length - 1));
+  reverse(nums, 0, (k - 1));
+  reverse(nums, k, (nums.length - 1));
 };
