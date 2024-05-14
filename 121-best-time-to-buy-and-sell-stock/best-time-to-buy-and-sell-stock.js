e8e543fe2c09eeb4e8f7885c9f4f872123edd7a6
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-13 22:50:58 -0700

    Time: 69 ms (79.33%) | Memory: 59 MB (60.76%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = (prices) => {
+  let left = 0;
+  let right = 1;
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
+};
