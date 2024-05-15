bd22354e8f91c9d8cd17b29c671480606cc34f60
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-14 21:51:01 -0700

    Time: 62 ms (62.52%) | Memory: 53.4 MB (43.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,11 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    // turns string to lowercase
     let lowerCased = s.toLowerCase()
+    // removes special characters
     let alphanumeric = lowerCased.replace(/[^a-z0-9]/g, '');
+    // removes spaces between characters
     let combined = alphanumeric.replace(/\s+/g, '');
+    // reverses string to compare
     let reversed = combined.split('').reverse().join('');
 
+    // if string is not equal to reversed string, return false
     if  (combined !== reversed) return false;
+    // returns true if statement returned true
     return true;
 };
