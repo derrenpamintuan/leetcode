c1a051381f435cc1d8f37e640fccb1a0ae81a717
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-18 18:44:53 -0700

    Time: 57 ms (95.97%) | Memory: 52.8 MB (52.50%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -4,10 +4,14 @@
  * @return {boolean}
  */
 var canConstruct = function (ransomNote, magazine) {
+    // loop over string
   for (let char of magazine) {
+    // replace the char from magazine in ransomNote with empty string
     ransomNote = ransomNote.replace(char, "");
   }
-  
+
+  // if ransomNote is empty return true
   if (!ransomNote) return true;
+  // otherwise return false
   else return false;
 };
