8927f211acba85f650cf5352fece9a75e7752c06
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-22 21:22:10 -0700

    Time: 70 ms (19.56%) | Memory: 51.5 MB (59.42%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -7,17 +7,24 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const hash = {};
-    let res = 0;
-    let majority = 0;
-
-    for (let n of nums) {
-        hash[n] = 1 + (hash[n] || 0);
-        if (hash[n] > majority) {
-            res = n;
-            majority = hash[n];
-        }
+    // declare a variable to store object
+    let counts = {};
+    // declare a variable and assign to null
+    let majority = null;
+    // iterate over the nums array
+    for (let i = 0; i < nums.length; i++) {
+        // assign index plus one or one to the counts variable of nums at index
+      counts[nums[i]] = counts[nums[i]] + 1 || 1
     }
 
-    return res;    
+    // iterate over the counts object
+    for (let key in counts) {
+        // if majority is equal to null or counts of majority is greater than counts of key
+      if (majority === null || counts[key] > counts[majority]) {
+        // assign key to majority
+        majority = key;
+      }
+    }
+    // return majority
+    return majority;
 };
