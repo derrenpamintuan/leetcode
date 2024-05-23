0eb36135c8167609a4c4fd695ac8f038f53f148e
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-22 21:35:57 -0700

    Time: 91 ms (20.77%) | Memory: 60.2 MB (24.90%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,16 +4,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
+    // create helper function to reverse array to check values
   function reverse(arr, start, end) {
+    // iterate over array while start index is less than end index
     while (start < end) {
+        // assign array at index of end and start to array at index of start and end
       [arr[start], arr[end]] = [arr[end], arr[start]];
+      // increment start
       start++;
+      // decrement end
       end--;
     }
   }
 
+    // assign remainder of the length of nums array to k
   k %= nums.length;
 
+    // use reverse fuction to to rotate nums array to the right k times
   reverse(nums, 0, (nums.length - 1));
   reverse(nums, 0, (k - 1));
   reverse(nums, k, (nums.length - 1));
