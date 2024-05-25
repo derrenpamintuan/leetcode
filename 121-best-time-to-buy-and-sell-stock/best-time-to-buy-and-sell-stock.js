89822655a0b5493b83bea0e565cb6c26544faacc
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-24 20:44:47 -0700

    Time: 72 ms (67.37%) | Memory: 59 MB (59.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,28 @@
  * @return {number}
  */
 const maxProfit = (prices) => {
-  let left = 0;
-  let right = 1;
-  let max_profit = 0;
-  while (right < prices.length) {
-    if (prices[left] < prices[right]) {
-      let profit = prices[right] - prices[left];
-
-      max_profit = Math.max(max_profit, profit);
-    } else {
-      left = right;
+    // assign start index to variable
+    let left = 0;
+    // assign iteration index to variable
+    let right = 1;
+    // assign a variable to track profit
+    let max_profit = 0;
+    // iterate over prices array
+    while (right < prices.length) {
+        // if prices at index of left is less than prices at index of right
+        if (prices[left] < prices[right]) {
+            // assign prices at index of right minus prices at index of left to a variable
+            let profit = prices[right] - prices[left];
+            // assign the higher number to max_profit variable
+            max_profit = Math.max(max_profit, profit);
+            // otherwise
+        } else {
+            // assign right to left variable
+            left = right;
+        }
+        // increment right variable
+        right++;
     }
-    right++;
-  }
-  return max_profit;
+    // return max_profit
+    return max_profit;
 };
