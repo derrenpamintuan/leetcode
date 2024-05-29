424c105a6f55829011cd0f384b8d745194fa9977
Derren Pamintuan <pamintuan2000@gmail.com>
2024-05-28 20:56:48 -0700

    Time: 63 ms (47.58%) | Memory: 51.1 MB (36.89%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -9,13 +9,21 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
+    // assign an empty obj for s letter count
     let objS={}
+    // assign an empty obj for t letter count
     let objT={}
-    for(let i=0;i<s.length;i++){
-        if(objS[s[i]] && (objS[s[i]]!==t[i])) return false
-        objS[s[i]]=t[i]
-        if(objT[t[i]] && (objT[t[i]]!==s[i])) return false
-        objT[t[i]]=s[i]
+    // iterate over the s string
+    for (let i = 0; i < s.length; i++){
+        // if key in objS and key in objS is not equal to character at index of t return false
+        if (objS[s[i]] && (objS[s[i]] !== t[i])) return false
+        // assign charcter at index of t to objS at key of s at index
+        objS[s[i]] = t[i]
+            // if key in objT and key in objT is not equal to character at index of s return false
+        if (objT[t[i]] && (objT[t[i]] !== s[i])) return false
+        // assign charcter at index of s to objT at key of t at index
+        objT[t[i]] = s[i]
     }
+    // return true
     return true
 };
