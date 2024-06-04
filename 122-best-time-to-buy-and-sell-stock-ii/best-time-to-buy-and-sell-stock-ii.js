9a6c3741f2fdb5375c8667cdce450a8430102a3e
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-03 20:03:00 -0700

    Time: 64 ms (15.58%) | Memory: 49.6 MB (31.11%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -7,19 +7,30 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    // assign 0 to variable to track output
     let output = 0;
+    // assign first element of prices array to variable to track current price
     let currentPrice = prices[0];
 
+    // iterate over prices array
     for (let i = 1; i < prices.length; i++) {
+        // assign prices at index to variable for readability
         const price = prices[i];
 
+        // if price is less than prices at index minus one
         if (price < prices[i - 1]) {
+            // subtract currentPrice from prices at index minus one then add and assign to ouput
             output += prices[i - 1] - currentPrice;
+            // assign price to currentPrice
             currentPrice = price;
         }
     }
 
-    if (prices[prices.length - 1] >= prices[prices.length - 2]) output += prices[prices.length - 1] - currentPrice;
-
+    // if prices at index of prices length minus one is greater than or equal to prices at index of prices length minus two
+    if (prices[prices.length - 1] >= prices[prices.length - 2]) {
+        // subract currentPrice from prices at index of prices length minus one then add and assign to output
+        output += prices[prices.length - 1] - currentPrice;
+    }
+    // return output
     return output;
 };
