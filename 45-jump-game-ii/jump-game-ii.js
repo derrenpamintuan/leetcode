e460a33c807a8312a0adecdebf53ff7f576e667c
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-10 22:07:29 -0700

    Time: 55 ms (70.49%) | Memory: 51.3 MB (35.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -4,11 +4,11 @@
  */
 var jump = function(nums) {
     let length = nums.length - 1;
-    let curr = -1;
+    let current = -1;
     let next = 0;
     let min = 0;
     for (let i = 0; next < length; i++) {
-        if (i > curr) min++, curr = next;
+        if (i > current) min++, current = next;
         next = Math.max(next, nums[i] + i);
     }
     return min;
