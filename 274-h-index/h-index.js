81d2235aec7231122004e6fa064a9c4e080bfcfd
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-14 15:38:41 -0700

    Time: 48 ms (86.79%) | Memory: 49.1 MB (50.66%) - LeetSync

--- 274-h-index/h-index.js
+++ 274-h-index/h-index.js
@@ -3,11 +3,17 @@
  * @return {number}
  */
 var hIndex = function(citations) {
+    // sort the citations array in ascending order
     citations.sort((a, b) => a - b)
+    // initialize the result variable to 0
     let result = 0;
+    // store the length of the citations array in variable n
     let n = citations.length;
+    // iterate over the sorted citations array
     for(let i = 0; i < n; i++){
+        // Line 6: Update the result to the maximum of the current result and the minimum of the current citation value and the number of papers that have at least this many citations (n-i)
         result = Math.max(result, Math.min(citations[i], n-i))
     }
+    // return the result
     return result;
 };
