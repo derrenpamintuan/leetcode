d523c63aaec2c2d0ce530fc9035554e5c0021dc4
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-19 19:49:54 -0700

    Time: 60 ms (68.47%) | Memory: 50.8 MB (43.05%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // Initialize an empty object to store number indices
     const pairIdx = {};
 
+    // Iterate over each element in the nums array
     for (let i = 0; i < nums.length; i++) {
+        // Get the current number
         const num = nums[i];
+        // Check if the complement (target - num) exists in pairIdx
         if (target - num in pairIdx) {
+            // If it exists, return the current index and the index of the complement
             return [i, pairIdx[target - num]];
         }
+        // Store the current number and its index in pairIdx
         pairIdx[num] = i;
     }    
 };
