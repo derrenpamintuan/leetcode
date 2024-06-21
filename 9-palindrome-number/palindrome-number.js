788df9f723987fcf52463e5d57ec525cbbe8bd68
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-21 15:00:42 -0700

    Time: 119 ms (87.51%) | Memory: 58.5 MB (27.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,14 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    var reverse = 0;
-    var copy = x;
-
-    while (copy > 0) {
-      const digit = copy % 10;
-      reverse = reverse * 10 + digit;
-      copy = ~~(copy / 10);
-    }
-
-    return reverse == x;
+    let str = x.toString();
+    let arr = str.split('');
+    let reversedArr = arr.reverse();
+    let reversedStr = reversedArr.join('');
+    return parseInt(reversedStr) === x;
 };
