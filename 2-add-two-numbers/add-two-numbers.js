7a4cf270fe94f5ddf5427cd9f44c869b0a1cb2dd
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-26 16:33:46 -0700

    Time: 111 ms (47.41%) | Memory: 54.7 MB (91.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -11,27 +11,43 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+    // Initialize a new ListNode and store it in variable dummy
     let dummy = new ListNode();
+    // Set res to point to dummy
     let res = dummy;
+    // Initialize total and carry to 0
     let total = 0, carry = 0;
 
+    //While l1 is not null OR l2 is not null OR carry is not 0:
     while (l1 || l2 || carry) {
+        // Set total to carry
         total = carry;
 
+        // If l1 is not null:
         if (l1) {
+            // Add l1's value to total
             total += l1.val;
+            // Move to the next node in l1
             l1 = l1.next;
         }
+        // If l2 is not null:
         if (l2) {
+            // Add l2's value to total
             total += l2.val;
+            // Move to the next node in l2
             l2 = l2.next;
         }
 
+        // Set num to total modulo 10
         let num = total % 10;
+        // Update carry to be the integer division of total by 10
         carry = Math.floor(total / 10);
+        // Create a new ListNode with value num and set dummy's next to this new node
         dummy.next = new ListNode(num);
+        // Move dummy to its next node
         dummy = dummy.next;
     }
 
+    // Return res's next node
     return res.next;    
 };
