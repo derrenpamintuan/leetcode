fb884b31c15cdac9c1d6eeb32ab240258a969104
Derren Pamintuan <pamintuan2000@gmail.com>
2024-06-27 16:58:07 -0700

    Time: 73 ms (90.22%) | Memory: 53.3 MB (70.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,19 +3,29 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    // Initialize left pointer to 0
     let left = 0;
+    // Initialize maxLength to 0
     let maxLength = 0;
+    // Initialize an empty set charSet
     let charSet = new Set();
 
+    // For each character in s with index right from 0 to length of s - 1:
     for (let right = 0; right < s.length; right++) {
+        // While charSet contains the character at index right:
         while (charSet.has(s[right])) {
+            // Remove the character at index left from charSet
             charSet.delete(s[left]);
+            // Increment left pointer by 1
             left++;
         }
 
+        // Add the character at index right to charSet
         charSet.add(s[right]);
+        // Update maxLength to be the maximum of maxLength and (right - left + 1)
         maxLength = Math.max(maxLength, right - left + 1);
     }
 
+    // Return maxLength
     return maxLength;    
 };
