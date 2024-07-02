81eb798518092c3e710551ebd5fc978f706d0828
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-01 22:42:06 -0700

    Time: 118 ms (16.91%) | Memory: 55.5 MB (53.20%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,12 +4,18 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+    // Merge the two input arrays
     let mergedA = nums1.concat(nums2);
+    // Sort the merged array in ascending order
     let sortedA = mergedA.sort(function (a, b) { return a - b });
+    //# Calculate the middle index
     let middleI = (sortedA.length - 1) / 2;
+    //Check if the length of the sorted array is even
     if (sortedA.length % 2 === 0) {
+        // Return the average of the two middle elements
       return (sortedA[middleI - .5] + sortedA[middleI + .5]) / 2;
     } else {
+        // Return the middle element
       return sortedA[middleI];
     }
 };
