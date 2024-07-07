caca3f82231d64343074096776509cadc92f5c1c
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-06 21:47:38 -0700

    Time: 45 ms (92.68%) | Memory: 48.4 MB (82.93%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.js
+++ 2645-pass-the-pillow/pass-the-pillow.js
@@ -3,9 +3,10 @@
  * @param {number} time
  * @return {number}
  */
-var passThePillow = function (n, time) {
-    let div = Math.floor(time / (n - 1));
-    let rem = time % (n - 1);
-
-    return div % 2 == 0 ? rem + 1 : n - rem;
+var passThePillow = function(n, time) {
+    // Calculate how many complete passes have occurred
+    let chunks = Math.floor(time / (n - 1));
+    // If the number of chunks is even, return the position of the pillow in the forward direction
+    // Otherwise, return the position of the pillow in the reverse direction
+    return chunks % 2 === 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
 };
