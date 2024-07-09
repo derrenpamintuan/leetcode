f294457d6c848f68bd35699e88f52d7429a26ec1
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-09 14:45:13 -0700

    Time: 81 ms (47.74%) | Memory: 55.8 MB (62.33%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
@@ -3,12 +3,18 @@
  * @return {string}
  */
 var firstPalindrome = function(words) {
+    // iterate over the words array
     for (let i = 0; i < words.length; i++) {
+        // assign words at index to word
         let word = words[i];
+        // reverse word by splitting between each character then use reverse method and join each character
         let reverse = word.split('').reverse().join('');
+        // if word is equal to reverse
         if (word === reverse) {
+            // return word
             return word;
         }
     }
+    // return empty string
     return '';
 };
