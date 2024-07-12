6060e2c3d2f6a6fa9047e6b3fd11a75d895e02d8
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-11 21:25:29 -0700

    Time: 60 ms (97.95%) | Memory: 52 MB (92.78%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,5 +3,8 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    return (n > 0) && ((n & (n - 1)) === 0);
+    // Check if n is not zero and if n is a power of two
+    // if n is not equal to 0 and (n AND (n - 1)) is equal to 0: return true
+    // otherwise return false
+    return n > 0 && ((n & (n - 1)) === 0);
 };
