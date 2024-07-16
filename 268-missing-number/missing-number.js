32a73a35a538a6f8f704bd4b73fc1306e62c75be
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-15 21:00:19 -0700

    Time: 80 ms (21.04%) | Memory: 51.4 MB (40.86%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,14 +3,22 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    // sort nums array and assign it to sortedNums
     const sortedNums = nums.sort((a, b) => a - b);
+    // assign 0 to missing
     let missing = 0;
+    // iterate over the sortedNus array
     for (let i = 0; i < sortedNums.length; i++) {
+        // if sortedNums at index is not equal to index
         if (sortedNums[i] !== i) {
+            // assign index to missing
             missing = i;
+            // break loop cycle
             break;
         }
+        // increment missing
         missing++
     }
+    // retiurn missing
     return missing;
 };
