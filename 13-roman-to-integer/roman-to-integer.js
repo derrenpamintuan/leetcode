667ba823e6e69e3b1ffe2a5805aeca2e05ffd58f
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-17 17:33:49 -0700

    Time: 122 ms (33.05%) | Memory: 54.6 MB (49.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-      const sym = {
+    // assign roman symbols and values to an object variable
+    const sym = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -13,19 +14,28 @@ var romanToInt = function(s) {
         'M': 1000
     }
 
+    // assign 0 to result variable
     let result = 0;
 
+    // iterate over the s string
     for (let i = 0; i < s.length; i++) {
+        // assign sym at curent value to cur variable
         const cur = sym[s[i]];
+        // // assign sym at next of current value to next variable
         const next = sym[s[i + 1]];
 
+        // if cur is less than next
         if (cur < next) {
+            // add and increment next minus cur to the result variable
             result += next - cur;
+            // increment i
             i++;
+            // otherwise
         } else {
+            // add and increment cur to result variable
             result += cur;
         }
     }
-
+    // return result
     return result;
 };
