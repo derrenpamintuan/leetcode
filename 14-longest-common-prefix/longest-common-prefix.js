e607fa05a4ee36f8203ca5de92b540054c763a71
Derren Pamintuan <pamintuan2000@gmail.com>
2024-07-22 16:29:08 -0700

    Time: 46 ms (92.81%) | Memory: 48.8 MB (90.38%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,19 +3,28 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+    // assign strs at index 0 to pref variable
     let pref = strs[0];
+    // assign the length of pref to prefLen variable
     let prefLen = pref.length;
 
+    // iterate over the strs array
     for (let i = 1; i < strs.length; i++) {
+        // assign index of stars to s variable
         let s = strs[i];
+        // while pref is not equal to s at substring of 0 to prefLen
         while (pref !== s.substring(0, prefLen)) {
+            // decerment prefLen variable
             prefLen--;
+            // if prefLeng is equal to 0
             if (prefLen === 0) {
+                // return an empty string
                 return "";
             }
+            // assign pref at substring of 0 to prefLeng to the pref variable
             pref = pref.substring(0, prefLen);
         }
     }
-
+    // return pref
     return pref;    
 };
