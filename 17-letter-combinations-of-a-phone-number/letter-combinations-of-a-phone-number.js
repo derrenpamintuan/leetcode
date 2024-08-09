73b94de7907c83846cb2dbca88173ff7ef567dcb
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-08 21:27:33 -0700

    Time: 44 ms (89.21%) | Memory: 48.2 MB (95.26%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -7,10 +7,13 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    // if digits string has no length
     if (!digits.length) {
+        // return empty array
         return [];
     }
     
+    // assign the correct with each number to digitsToLetters variable
     const digitToLetters = {
         '2': 'abc',
         '3': 'def',
@@ -22,20 +25,29 @@ var letterCombinations = function(digits) {
         '9': 'wxyz'
     };
     
+    // assign empty array to res variable
     const res = [];
     
+    // create backtrack function with the parameters index and combination 
     function backtrack(idx, comb) {
+        // if index is equal to digits length
         if (idx === digits.length) {
+            // ass combination to res array
             res.push(comb);
+            // return function
             return;
         }
         
+        // iterate over digitsToLetters at digits at index
         for (const letter of digitToLetters[digits[idx]]) {
+            // use backtrack function with the arguments idx plus one and comb plus letter
             backtrack(idx + 1, comb + letter);
         }
     }
     
+    // use backtrack function with the arguments 0 and empty string
     backtrack(0, "");
     
+    // return res array
     return res;    
 };
