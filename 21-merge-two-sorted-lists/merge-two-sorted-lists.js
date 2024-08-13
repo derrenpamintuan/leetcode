23fa2eb8d635bc9d1ed9efd267d934d00cc50973
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-13 15:13:46 -0700

    Time: 52 ms (94.32%) | Memory: 51.6 MB (51.54%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,13 +11,20 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
+    // if list1 is empty, return list2
     if (!list1) { return list2 }
+    // if list2 is empty, return list1
     if (!list2) { return list1 }
+    // if value at list1 is less than or equal to value at list2
     if (list1.val <= list2.val) {
+        // call mergeTwoLists function with next value of list1 and list2 and assign it to next value of list1
         list1.next = mergeTwoLists(list1.next, list2);
+        // return list1
         return list1;
     }
+    // call mergeTwoLists function with list1 and next value of list2 and assign it to next value of list2
     list2.next = mergeTwoLists(list1, list2.next);
+    // return list2
     return list2;
 
 };
