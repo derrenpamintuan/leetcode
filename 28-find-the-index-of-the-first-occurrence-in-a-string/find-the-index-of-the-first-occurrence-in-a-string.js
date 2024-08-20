881aee30b33ee4fc15e0c27622310340a51f4b66
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-19 17:57:31 -0700

    Time: 39 ms (97.97%) | Memory: 48.9 MB (43.96%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,7 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    // return first occurence of needle in haystack
+    // returns -1 if not found
     return haystack.indexOf(needle);
 };
