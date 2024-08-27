85992dbe94da24ff1b29e7ef5bf4e65274ae1af4
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-26 18:35:34 -0700

    Time: 49 ms (77.81%) | Memory: 49.7 MB (56.19%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,16 +4,24 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
+    // if target is not found in nums, return [-1, -1]
     if (nums.indexOf(target) === -1) { return [-1, -1] };
 
+    // assign empty array to res
     let res = [];
+    // push index of target in nums to res array
     res.push(nums.indexOf(target));
 
+    // iterate over nums array from the end to start
     for (let i = nums.length - 1; i >= 0; i--) {
+        // if nums at index is strictly equal to target
         if (nums[i] === target) {
+            // push index to res array
             res.push(i);
+            // end iteration
             break;
         }
     }
+    // return res array
     return res;
 };
