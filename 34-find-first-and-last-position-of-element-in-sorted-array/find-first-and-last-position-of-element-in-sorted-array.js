353a116dde7bb8378a5f9b0e3fb0ad989e7e8d0b
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-26 18:40:47 -0700

    Time: 50 ms (72.94%) | Memory: 50.1 MB (18.35%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,16 +7,16 @@ var searchRange = function(nums, target) {
     // if target is not found in nums, return [-1, -1]
     if (nums.indexOf(target) === -1) { return [-1, -1] };
 
-    // assign empty array to res
+    // assign empty array to res variable
     let res = [];
-    // push index of target in nums to res array
+    // push the index of target in nums to res array
     res.push(nums.indexOf(target));
 
-    // iterate over nums array from the end to start
+    // iterate over nums array from end to start
     for (let i = nums.length - 1; i >= 0; i--) {
-        // if nums at index is strictly equal to target
+        // if index of nums is strictly equal to target
         if (nums[i] === target) {
-            // push index to res array
+            // push the index to res array
             res.push(i);
             // end iteration
             break;
