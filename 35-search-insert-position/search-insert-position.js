a1b061f3d759e98f0563f37085324c1a5ff4a611
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-28 17:03:17 -0700

    Time: 48 ms (78.28%) | Memory: 50.6 MB (5.29%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -4,10 +4,14 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+    // if target is not found in nums array
     if (nums.indexOf(target) === -1) {
+        // add target to nums array
         nums.push(target);
+        // sort nums array in ascending order
         nums.sort((a, b) => a - b);
     }
 
+    // return index of target in nums array
     return nums.indexOf(target);
 };
