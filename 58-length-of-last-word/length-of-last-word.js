754a1b7f7f1a858b120206dfcbca8e7611c2349b
Derren Pamintuan <pamintuan2000@gmail.com>
2024-08-30 15:31:03 -0700

    Time: 51 ms (58.93%) | Memory: 48.7 MB (78.07%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,12 +3,18 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+    // remove white space around s string and assign to trimmed
     let trimmed = s.trim();
+    // assign 0 to length
     let length = 0;
 
+    // iterate over trimmed string from end to start
     for (let i = trimmed.length - 1; i >= 0; i--) {
+        // if index of trimmed does not equal a space, increment length
         if (trimmed[i] !== ' ') { length++ }
+        // otherwise end iteration
         else { break };
     }
+    // return length variable
     return length;
 };
