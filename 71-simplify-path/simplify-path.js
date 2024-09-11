e203e8d7a4d71cd34066a6dbd8f87bc4891f70e8
Derren Pamintuan <pamintuan2000@gmail.com>
2024-09-10 23:50:21 -0700

    Time: 66 ms (23.72%) | Memory: 51.5 MB (22.34%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -3,18 +3,29 @@
  * @return {string}
  */
 var simplifyPath = function (path) {
+    // assign empty array to stack
     const stack = [];
+    // split path with / and assign to directories
     const directories = path.split("/");
+    // // iterate over directories
     for (const dir of directories) {
+        // if dir is equal to . or not dir
         if (dir === "." || !dir) {
+            // continue iteration
             continue;
+            // otherwise if dir is equal to ..
         } else if (dir === "..") {
+            // if the length of stack is 0
             if (stack.length > 0) {
+                // remove last element
                 stack.pop();
             }
+            // otherwise
         } else {
+            // push dir into stack
             stack.push(dir);
         }
     }
+    // return / plus stack joined with /
     return "/" + stack.join("/");
 };
